0d88f79344fa4797f520549b1b179f17583fe03c
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-01-27 00:42:34 -0300

    imports

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export * from './shared/types';
+
+// Hooks
+
+export * from './hooks/useOutClick';
+
+// Utilities
+
+export * from './utilities/hasNodeInDOM';
+
+// Resources
+
+export * from './resources/mergeRefs';
+export * from './resources/mergeClassNames';
