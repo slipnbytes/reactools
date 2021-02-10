8fd2466aa08ff849de6311f9bd59336dadaee74c
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-02-09 23:11:13 -0300

    chore: create `useCurrentRef` hook

---
+++ src/hooks/useCurrentRef.ts
@@ -0,0 +1,20 @@
+import { useState, useCallback, RefCallback } from 'react';
+
+import type { RefValue } from '@/shared/types';
+
+export type UseCurrentRefManager<T> = [RefValue<T>, RefCallback<T>];
+
+export function useCurrentRef<T = any>(
+  defaultValue: RefValue<T> = null,
+): UseCurrentRefManager<T> {
+  const [currentValue, setCurrentValue] = useState<RefValue<T>>(defaultValue);
+
+  const setCurrentRefValue = useCallback<RefCallback<T>>(
+    (value: T | null): void => {
+      setCurrentValue(value);
+    },
+    [],
+  );
+
+  return [currentValue, setCurrentRefValue];
+}
