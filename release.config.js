4748235e4893453a5564c4afcd653b68c83eb7ef
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-02-11 19:51:00 -0300

    chore: update release config [skip ci]

--- release.config.js
+++ release.config.js
@@ -1,3 +1,40 @@
+/* eslint-disable no-template-curly-in-string */
+
 module.exports = {
   branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        tarballDir: '.tarball',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        assets: [
+          'CHANGELOG.md',
+          'package.json',
+          'package-lock.json',
+          'npm-shrinkwrap.json',
+        ],
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: '.tarball/*.tgz',
+      },
+    ],
+  ],
 };
