bd33afe7a41387e845b5eebeb2b783d58a3fab20
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-02-13 20:39:45 -0300

    chore: create more tests

--- src/utilities/internal/__tests__/object.test.ts
+++ src/utilities/internal/__tests__/object.test.ts
@@ -1,6 +1,15 @@
-import { pick, hasOwnProperty } from '../object';
+import { pick, update, hasOwnProperty, makeObjectByKey } from '../object';
 
 describe('object', () => {
+  test('makeObjectByKey()', () => {
+    const object = makeObjectByKey('key', 'value');
+
+    expect(hasOwnProperty(object, 'key')).toBe(true);
+    expect(object).toEqual({ key: 'value' });
+
+    expect(object.key).toBe('value');
+  });
+
   test('pick()', () => {
     const object = {};
 
@@ -22,5 +31,48 @@ describe('object', () => {
     object.user.name = 'name';
 
     expect(hasOwnProperty(object, 'user.name')).toBe(true);
+  });
+
+  test('update()', () => {
+    let object;
+    let newObject;
+
+    object = {
+      age: 10,
+      username: 'username',
+    };
+
+    newObject = update(object, {
+      username: 'new-username',
+    });
+
+    expect(newObject.age).toBe(10);
+    expect(newObject.username).toBe('new-username');
+
+    object = {
+      user: {
+        username: 'username',
+      },
+    };
+
+    newObject = update(object, {
+      user: {
+        username: 'new-username',
+      },
+    });
+
+    expect(newObject.user.username).toBe('new-username');
+
+    newObject = update(object, {
+      user: undefined,
+    });
+
+    expect(newObject.user.username).toBe('username');
+
+    newObject = update(object, {
+      user: (null as unknown) as undefined,
+    });
+
+    expect(newObject.user).toEqual(object.user);
   });
 });
