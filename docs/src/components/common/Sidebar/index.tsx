2b7d1735078c3e9debd42bcfa9be3ade8ed48b49
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-02-19 01:29:32 -0300

    chore: make sidebar responsive
    Co-authored-by: Gabriel Hijazi <contato@devhijazi.com.br>

--- docs/src/components/common/Sidebar/index.tsx
+++ docs/src/components/common/Sidebar/index.tsx
@@ -1,29 +1,72 @@
-import React from 'react';
+import { mergeClassNames, useOutClick } from '@hitechline/react';
+import React, { useEffect } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdClose } from 'react-icons/md';
 
 import { Link } from '@components/forward/Link';
 
 import { documentationSections } from '@fixtures/data/documentation-sections';
+import { useUI } from '@fixtures/hooks/useUI';
 
 import styles from './styles.module.css';
 
-/* eslint-disable import-helpers/order-imports */
-import { mergeClassNames } from '../../../../..';
-
 export const Sidebar = () => {
-  return (
-    <div className={styles.container}>
-      <div className={mergeClassNames('main-container-base', styles.content)}>
-        {documentationSections.map(({ title, links }) => (
-          <div>
-            {title && <h4>{title}</h4>}
+  const { ref, addListener, removeListener } = useOutClick<HTMLDivElement>();
+  const { sidebarOpened, closeSidebar, openSidebar } = useUI();
 
-            <section>
-              {links.map(({ href, title: linkTitle }) => (
-                <Link href={href}>{linkTitle}</Link>
-              ))}
-            </section>
-          </div>
-        ))}
+  useEffect(() => {
+    addListener(closeSidebar);
+
+    return () => {
+      removeListener(closeSidebar);
+    };
+  }, [addListener, removeListener, closeSidebar]);
+
+  return (
+    <div ref={ref} className={styles.wrap}>
+      <div
+        className={mergeClassNames('main-container-base', styles.responsive)}
+      >
+        <button
+          type="button"
+          onClick={openSidebar}
+          className="flex items-center"
+        >
+          <GiHamburgerMenu size="4rem" />
+          <span className="ml-3">MENU</span>
+        </button>
+      </div>
+
+      <div
+        className={mergeClassNames(styles.container, {
+          opened: sidebarOpened,
+        })}
+      >
+        <button
+          type="button"
+          onClick={closeSidebar}
+          className={mergeClassNames(
+            'flex',
+            'items-center',
+            styles.close__button,
+          )}
+        >
+          <MdClose size="4rem" />
+        </button>
+
+        <div className={mergeClassNames('main-container-base', styles.content)}>
+          {documentationSections.map(({ title, links }) => (
+            <div>
+              {title && <h4>{title}</h4>}
+
+              <section>
+                {links.map(({ href, title: linkTitle }) => (
+                  <Link href={href}>{linkTitle}</Link>
+                ))}
+              </section>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
