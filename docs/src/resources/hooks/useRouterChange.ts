785e32fcd66e984402c732c37407e117cbfaca8b
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-02-20 16:10:20 -0300

    chore: close sidebar after navigation

---
+++ docs/src/resources/hooks/useRouterChange.ts
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export function useRouterChange(listener: () => any): void {
+  const { events } = useRouter();
+
+  useEffect(() => {
+    events.on('routeChangeComplete', listener);
+
+    return () => {
+      events.off('routeChangeComplete', listener);
+    };
+  }, [listener]);
+}
