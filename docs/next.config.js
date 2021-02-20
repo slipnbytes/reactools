b3971dcecd08133fad1ade9529dd545b46b349c7
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-02-20 16:11:58 -0300

    chore: optimize static files

--- docs/next.config.js
+++ docs/next.config.js
@@ -33,5 +33,40 @@ module.exports = composePlugins(
     devIndicators: {
       autoPrerender: false,
     },
+    images: {
+      domains: ['github.com', 'hitechline.com.br'],
+    },
+    headers: () => [
+      {
+        source: '/fonts/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:
+              'public, max-age=604800, s-maxage=604800, must-revalidate, proxy-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/(img|icons)/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:
+              'public, max-age=86400, s-maxage=86400, must-revalidate, proxy-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/css/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:
+              'public, max-age=21600, s-maxage=21600, must-revalidate, proxy-revalidate',
+          },
+        ],
+      },
+    ],
   },
 );
