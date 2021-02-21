6ef815fb8797be627113b0c2a3c6bc912ff36862
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-02-20 21:30:46 -0300

    chore: fix postcss configuration

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,20 +1,40 @@
+const { join, resolve } = require('path');
+
+const BASE_PATH = resolve(__dirname);
+const CSS_ROOT_PATH = join(BASE_PATH, 'src', 'styles');
+
 module.exports = {
   plugins: [
     'tailwindcss',
     'postcss-nesting',
-    'postcss-flexbugs-fixes',
     [
       'postcss-preset-env',
       {
+        stage: 3,
         autoprefixer: {
           flexbox: 'no-2009',
         },
-        stage: 3,
-        features: {
-          'color-mod-function': {
-            unresolved: 'warn',
-          },
-        },
+      },
+    ],
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-custom-properties',
+      {
+        preserve: true,
+      },
+    ],
+    [
+      'postcss-color-converter',
+      {
+        outputColorFormat: 'hsl',
+      },
+    ],
+    'postcss-color-hsl',
+    [
+      'postcss-color-mod-function',
+      {
+        unresolved: 'warn',
+        importFrom: [join(CSS_ROOT_PATH, 'root.css')],
       },
     ],
   ],
