02520671ec89f354340abc963ab67e5596ed82c3
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-03-15 02:11:31 -0300

    chore: refactor eslint config using `@hitechline` configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,103 +7,38 @@ const { getWorkspacesPackages } = require('./shared/getWorkspacesPackages');
 const packages = getWorkspacesPackages(false);
 
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    'import',
-    'prettier',
-    'import-helpers',
-    'testing-library',
-    '@typescript-eslint',
-  ],
+  plugins: ['import-helpers', 'testing-library'],
   extends: [
-    'airbnb',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:testing-library/react',
     'plugin:testing-library/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
+    'plugin:testing-library/react',
+    '@hitechline/eslint-config/web',
+    '@hitechline/eslint-config/typescript',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  env: {
-    es6: true,
-    jest: true,
-    browser: true,
-  },
   parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
     tsconfigRootDir: ROOT_PATH,
     project: 'tsconfig.eslint.json',
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
   settings: {
-    'import/extensions': ['.js', '.ts', '.tsx'],
-    'react': {
-      version: 'detect',
-    },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.js', '.ts', '.tsx'],
-    },
     'import/resolver': {
       typescript: {
         alwaysTryTypes: true,
         project: packages
-          .filter(package =>
-            existsSync(join(ROOT_PATH, package, 'tsconfig.json')),
+          .filter(packageName =>
+            existsSync(join(ROOT_PATH, packageName, 'tsconfig.json')),
           )
-          .map(package => `${package}/tsconfig.json`),
+          .map(packageName => `${packageName}/tsconfig.json`),
       },
     },
   },
-  overrides: [
-    {
-      files: ['**/*.js'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off',
-      },
-    },
-  ],
+
   rules: {
-    'camelcase': 'off',
-    'global-require': 'off',
-    'prettier/prettier': 'error',
-    'class-methods-use-this': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
 
-    'no-shadow': 'off',
-    'no-use-before-define': 'off',
-
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/jsx-filename-extension': [
-      'warn',
-      {
-        extensions: ['js', '.jsx', '.tsx'],
-      },
-    ],
-
-    'import/prefer-default-export': 'off',
+    'import/order': 'off',
     'import/no-extraneous-dependencies': [
       'error',
       {
         packageDir: [ROOT_PATH, ...packages],
-      },
-    ],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        ts: 'never',
-        tsx: 'never',
       },
     ],
     'import-helpers/order-imports': [
@@ -120,25 +55,6 @@ module.exports = {
           ['parent', 'sibling', 'index'],
           '/types$/',
         ],
-      },
-    ],
-
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
-    '@typescript-eslint/prefer-nullish-coalescing': ['error'],
-    '@typescript-eslint/no-empty-interface': [
-      'error',
-      {
-        allowSingleExtends: true,
-      },
-    ],
-    '@typescript-eslint/no-use-before-define': [
-      'error',
-      {
-        variables: false,
-        functions: false,
-        ignoreTypeReferences: true,
       },
     ],
   },
