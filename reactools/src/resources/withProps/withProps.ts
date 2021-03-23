a62f8c9580d18087ed38e8f9cbc97096135e2264
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-03-23 02:07:08 -0300

    chore(withProps): improve types

--- reactools/src/resources/withProps/withProps.ts
+++ reactools/src/resources/withProps/withProps.ts
@@ -1,9 +1,10 @@
-import { ComponentType, FunctionComponent } from 'react';
+import type { ComponentType, FunctionComponent } from 'react';
 
 import { createFactory } from './WithPropsComponent';
 
 import type {
   AnyObject,
+  ComponentBase,
   GetComponentProps,
   GetComponentReturnType,
 } from '@/shared/types';
@@ -18,14 +19,29 @@ type MergeProps<Default extends AnyObject, Provided extends AnyObject> = Pick<
 > &
   Partial<Pick<Default, keyof Provided>>;
 
-export interface WithPropsReturn<P extends AnyObject> {
-  <T extends FunctionComponent<any>>(Component: T): (
+export interface WithPropsOtherComponent<
+  T extends ComponentType<any>,
+  P extends AnyObject
+> extends ComponentBase<GetComponentProps<T>> {
+  (props: MergeProps<GetComponentProps<T>, P>): GetComponentReturnType<T>;
+}
+
+export interface WithPropsFunctionComponent<
+  T extends FunctionComponent<any>,
+  P extends AnyObject
+> extends ComponentBase<GetComponentProps<T>> {
+  (
     props: MergeProps<GetComponentProps<T>, P>,
     ...restParameters: RemovePropsParameterAndGetRestParameters<T>
-  ) => GetComponentReturnType<T>;
-  <T extends ComponentType<any>>(Component: T): (
-    props: MergeProps<GetComponentProps<T>, P>,
-  ) => GetComponentReturnType<T>;
+  ): GetComponentReturnType<T>;
+}
+
+export interface WithPropsReturn<P extends AnyObject> {
+  <T extends FunctionComponent<any>>(Component: T): WithPropsFunctionComponent<
+    T,
+    P
+  >;
+  <T extends ComponentType<any>>(Component: T): WithPropsOtherComponent<T, P>;
 }
 
 export function withProps<P extends AnyObject>(props: P): WithPropsReturn<P> {
